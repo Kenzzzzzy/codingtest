f58ce50bc17230b445a05567ce15c5f671de0188
kenzzzzzy <kenzy1828179@gmail.com>
2023-12-30 03:45:37 +0000

    エラーテスト実装

--- app/analyze.py
+++ app/analyze.py
@@ -9,32 +9,38 @@ def analyze_image(path):
         'image_path': path
     }
     
-    request_time = datetime.now() 
     
     # 指定リトライ数だけループ
     for i in range(config.max_retries):
         try:
+            request_time = datetime.now().timestamp()
+
             response = requests.post(config.API_URL, data=form_data, timeout=config.api_timeout)
             
+            response_time = datetime.now().timestamp()
+
             # コールエラーを確認
             response.raise_for_status()
             
-            response_time = datetime.now()
-    
             # レスポンスに時刻追加
             response_json = response.json()
-            response_json['resquest_time'] = request_time.isoformat()
-            response_json['response_time'] = response_time.isoformat()
-            
+            response_json['resquest_time'] = int(request_time)
+            response_json['response_time'] = int(response_time)
+
+            print(f'レスポンス：{response_json}')
             return response_json
             
         except requests.Timeout:
-            print(f'リトライ中：{i}回')
-            # タイムアウトなので少し間を開ける
-            time.sleep(5)
+            if i == config.max_retries - 1:
+                print('タイムアウト')
+                raise
+            else:
+                # タイムアウトなので少し間を開ける
+                time.sleep(1)
+                print(f'リトライ：{i+1}回')
         except requests.HTTPError as http_err:
-            print(f'APIエラー；{http_err}')
-            break
+            print(f'APIエラー：{http_err.response.status_code}')
+            raise
         except Exception as e:
             print(f'予期しないエラー；{e}')
-            break
+            raise
--- tests/test_analyze.py
+++ tests/test_analyze.py
@@ -28,3 +28,48 @@ def test_analyze_image_success(mocker):
     assert 'response_time' in response  
 
     mock_post.assert_called_once_with(config.API_URL, data={'image_path': 'images/test.jpg'}, timeout=config.api_timeout)
+    
+    
+def test_analyze_image_error(mocker):
+    # Analyze内で呼ばれるAPIのモック生成
+    mock_post = mocker.patch('requests.post')
+    
+    # APIの想定戻り値
+    mock_response = MagicMock()
+    mock_response.json.return_value = {
+        'success': False,
+        'message': 'Error:E50012',
+        'estimated_data': {
+        }
+    }
+    mock_response.raise_for_status.return_value = None
+    mock_post.return_value = mock_response
+    
+    # テスト実行
+    response = analyze_image('images/test2.jpg')
+    assert response['success'] == False
+    mock_post.assert_called_once_with(config.API_URL, data={'image_path': 'images/test2.jpg'}, timeout=config.api_timeout)
+
+
+def test_analyze_image_timeout(mocker):
+    # タイムアウトするよう指定
+    mocker.patch('requests.post', side_effect=requests.Timeout)
+
+    # 例外が発生することを確認
+    with pytest.raises(requests.Timeout):
+        analyze_image("images/test.jpg")        
+
+
+def test_analyze_image_500error(mocker):
+    mock_post = mocker.patch('requests.post')
+    
+    # ステータスコード設定
+    mock_response = MagicMock()
+    mock_response.status_code = 500
+    mock_response.raise_for_status.side_effect = requests.HTTPError(response=mock_response)
+    
+    mocker.patch('requests.post', return_value=mock_response)
+    
+    with pytest.raises(requests.HTTPError):
+        analyze_image("images/test.jpg")
+    
